a3aa545e55aa4331e5378d106f345ee502364643
Vitaly Vorobyev <vitaly.vorobyev@cern.ch>
2021-06-14 21:23:24 +0700

    magic fixed

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <iomanip>
+#include <functional>
+#include <optional>
 
 import definitions;
 import magicfinder;
@@ -12,34 +14,35 @@ std::ostream& operator<<(std::ostream& os, const def::Color& c) {
     return os << "Black";
 }
 
+void for_each_square(std::function<void(size_t, size_t)> cb) {
+    for (size_t row = 0; row < def::BOARD_SIZE; ++row)
+        for (size_t col = 0; col < def::BOARD_SIZE; ++col)
+            cb(row, col);
+}
+
+void print_if_has_value(std::ostream& os, size_t row, size_t col, std::optional<Bitboard>& magic) {
+    os << "row " << row << " col " << col << ": ";
+    if (magic.has_value()) {
+        os << "0x" << std::hex << magic.value() << '\n';
+    } else {
+        os << "No magic today" << '\n';
+    }
+}
+
 int main() {
     MagicFinder mf;
 
     std::cout << "Rook magic\n";
-    for (size_t row = 0; row < def::BOARD_SIZE; ++row) {
-        for (size_t col = 0; col < def::BOARD_SIZE; ++col) {
-            auto magic = mf.find_rook_magic({1, 3});
-            std::cout << "row " << row << " col " << col << ": ";
-            if (magic.has_value()) {
-                std::cout << "0x" << std::hex << magic.value() << '\n';
-            } else {
-                std::cout << "No magic today" << '\n';
-            }
-        }
-    }
+    for_each_square([&mf](size_t row, size_t col) {
+        auto magic = mf.find_rook_magic({row, col});
+        print_if_has_value(std::cout, row, col, magic);
+    });
 
     std::cout << "\nBishop magic\n";
-    for (size_t row = 0; row < def::BOARD_SIZE; ++row) {
-        for (size_t col = 0; col < def::BOARD_SIZE; ++col) {
-            auto magic = mf.find_bishop_magic({1, 3});
-            std::cout << "row " << row << " col " << col << ": ";
-            if (magic.has_value()) {
-                std::cout << "0x" << std::hex << magic.value() << '\n';
-            } else {
-                std::cout << "No magic today" << '\n';
-            }
-        }
-    }
+    for_each_square([&mf](size_t row, size_t col) {
+        auto magic = mf.find_bishop_magic({row, col});
+        print_if_has_value(std::cout, row, col, magic);
+    });
 
     return 0;
 }
